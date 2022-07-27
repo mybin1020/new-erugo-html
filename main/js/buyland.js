0ee092808d683b46c4568dda54ef0d931ecfe889
Subin Choi <mybin1020@gmail.com>
2022-07-27 09:21:08 +0900

    buyland Js 수정중..

--- main/js/buyland.js
+++ main/js/buyland.js
@@ -40,39 +40,122 @@ for (let i = 0; i < 6; i++) {
 
 // mouse scroll
 window.onload = function () {
+  // const slider = document.querySelector(".buylandContent");
+  // const innerSlider = document.querySelector(".storys");
+  const window_width = window.innerWidth;
   const elm = document.querySelectorAll(".storys > li");
-  const elm_width = document.querySelector(".buylandContent");
+  const elm_height = document.querySelector(".storys");
+  const pageNum = document.querySelector(".pageNum");
   const elmCount = elm.length;
-  elm.forEach(function (item, index) {
-    item.addEventListener("mousewheel", function (event) {
-      event.preventDefault();
-      let delta = 0;
+  let mouse_index = 0;
+  // let pressed = false;
+  // let startX, x;
 
-      if (!event) event = window.event;
+  // scroll event
+  if (window_width > 920) {
+    elm.forEach(function (item, index) {
+      item.addEventListener("mousewheel", function (event) {
+        event.preventDefault();
+        let delta = 0;
 
-      if (event.wheelDelta) {
-        delta = event.wheelDelta / 120;
-        if (window.opera) delta = -delta;
-      } else if (event.detail) delta = -event.detail / 3;
+        if (!event) event = window.event;
 
-      let moveTop = window.scrollX;
-      let elmSelector = elm[index];
-      let location = elmSelector.clientWidth;
+        if (event.wheelDelta) {
+          delta = event.wheelDelta / 120;
+          if (window.opera) delta = -delta;
+        } else if (event.detail) delta = -event.detail / 3;
 
-      // wheel down : move to next section
-      if (delta < 0) {
-        if (elmSelector !== elmCount - 1) {
-          window.scrollTo({ left: location, behavior: "smooth" });
-          console.dir(elmSelector);
+        let elmSelector = elm[index];
+        let location = elmSelector.clientWidth;
+        // wheel down : move to next section
+        //console.log(location * mouse_index);
+        if (delta < 0) {
+          ++mouse_index;
+          if (mouse_index > elmCount - 1) {
+            mouse_index = 0;
+            window.scrollTo({
+              left: mouse_index,
+              behavior: "smooth",
+            });
+          }
+          if (elmSelector !== elmCount) {
+            window.scrollTo({
+              left: location * mouse_index,
+              behavior: "smooth",
+            });
+          }
+          pageNum.innerText = `${mouse_index + 1} / 7`;
         }
-      }
+        // wheel up : move to previous section
+        else {
+          --mouse_index;
+          if (mouse_index < 0) {
+            mouse_index = elmCount - 1;
+          }
 
-      // wheel up : move to previous section
-      else {
-        if (elmSelector !== 0) {
-          window.scrollTo({ left: -location, behavior: "smooth" });
+          if (elmSelector !== 0) {
+            window.scrollTo({
+              left: location * mouse_index,
+              behavior: "smooth",
+            });
+          }
+
+          pageNum.innerText = `${mouse_index + 1} / 7`;
         }
-      }
+      });
     });
-  });
+  } else if (window_width <= 920) {
+    const scroll_height = elm_height.clientHeight / elmCount;
+    elm_height.addEventListener("scroll", function () {
+      window.scrollTo({
+        top: 1000,
+        behavior: "smooth",
+      });
+    });
+  }
+
+  // img
+
+  // drag event
+  // slider.addEventListener("mousedown", function (e) {
+  //   pressed = true;
+  //   startX = e.offsetX - innerSlider.offsetLeft;
+  //   slider.style.cursor = "grabbing";
+  // });
+
+  // slider.addEventListener("mouseenter", function () {
+  //   slider.style.cursor = "grab";
+  // });
+
+  // slider.addEventListener("mouseup", function () {
+  //   slider.style.cursor = "grab";
+  // });
+
+  // window.addEventListener("mouseup", function () {
+  //   pressed = false;
+  // });
+
+  // slider.addEventListener("mousemove", function (e) {
+  //   if (!pressed) return;
+  //   e.preventDefault();
+  //   x = e.offsetX;
+  //   console.log(x - startX);
+
+  //   innerSlider.style.left = `${x - startX}px`;
+  //   checkboundary();
+  // });
+
+  // function checkboundary() {
+  //   let outer = slider.getBoundingClientRect();
+  //   let inner = innerSlider.getBoundingClientRect();
+  //   if (parseInt(innerSlider.style.left) > 0) {
+  //     innerSlider.style.left = "0px";
+  //   } else if (inner.right < outer.right) {
+  //     window.scrollTo({
+  //       left: location * mouse_index,
+  //       behavior: "smooth",
+  //     });
+  //     innerSlider.style.left = `-${inner.width - outer.width}px`;
+  //   }
+  // }
 };
